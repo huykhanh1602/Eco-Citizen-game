df442033c4d4e097e46fb3cfb88ab301b996b367
huykhanh1602 <24020179@vnu.edu.vn>
2026-04-21 16:10:27 +0700

    Move useState and useEffect hooks inside the Home component for proper scope

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,15 @@
 import Image from "next/image";
 import { useEffect, useState } from "react";
 
-// Trong component của xám cưng
-const [message, setMessage] = useState("");
-
-useEffect(() => {
-    fetch("/api/py/status")
-        .then((res) => res.json())
-        .then((data) => setMessage(data.message));
-}, []);
-
 export default function Home() {
+    // Trong component của xám cưng
+    const [message, setMessage] = useState("");
+
+    useEffect(() => {
+        fetch("/api/py/status")
+            .then((res) => res.json())
+            .then((data) => setMessage(data.message));
+    }, []);
     return (
         <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
             <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
